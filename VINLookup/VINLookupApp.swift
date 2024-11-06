e7359c7d8f7fd32dabd7f2e64034fbef6858ebf2
Staszek Stryjewski <s.stryjewski@icloud.com>
2024-11-06 12:58:10 +0100

    Initial Commit

---
+++ VINLookup/VINLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  VINLookupApp.swift
+//  VINLookup
+//
+//  Created by Stanisław Stryjewski on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VINLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
